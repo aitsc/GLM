6d299d3d219baeb8bc2c873a8133731a082877c7
diy <tsctan@foxmail.com>
2022-07-21 17:38:24 +0800

    调整

--- scripts/finetune_blank.sh
+++ scripts/finetune_blank.sh
@@ -9,7 +9,7 @@ source $1    # Model
 source $2    # Task
 
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
-DISTRIBUTED_ARGS="--nproc_per_node 4 --nnodes 1 --node_rank 0 --master_addr localhost --master_port $MASTER_PORT"
+DISTRIBUTED_ARGS="--nproc_per_node 1 --nnodes 1 --node_rank 0 --master_addr localhost --master_port $MASTER_PORT"
 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS finetune_glm.py \
        --finetune \
